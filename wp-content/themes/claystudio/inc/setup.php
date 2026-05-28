b8aa8111800bdbc3d471354e3030fa8eb548a392
YoG-Anish <yogesh.anish01@gmail.com>
2026-05-28 14:16:36 +0545

    file structure and woocommerce

---
+++ wp-content/themes/claystudio/inc/setup.php
@@ -0,0 +1,22 @@
+<?php 
+
+//register menu
+function register_my_menus() {
+    register_nav_menus(
+        array(
+            'header-menu' => __( 'Header Menu' ),
+            'footer-menu' => __( 'Footer Menu' ),
+        )
+    );
+}
+add_action( 'init', 'register_my_menus' );
+
+
+//theme support
+function claystudio_theme_support() {
+    add_theme_support( 'post-thumbnails' );
+    //woocommerce
+    add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'claystudio_theme_support' );
+
