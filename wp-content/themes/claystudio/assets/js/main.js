705f3e5a26bfe00dff1a8b21f201c4dd3bf4e423
Sabin Prajapati <sabinprajapati7@gmail.com>
2026-05-29 12:36:56 +0545

    trusted section

--- wp-content/themes/claystudio/assets/js/main.js
+++ wp-content/themes/claystudio/assets/js/main.js
@@ -46,6 +46,42 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     announcementSplide.mount(window.splide.Extensions);
   }
+  // Go to TOp
+  const progressPath = document.querySelector(".progress-wrap path");
+  const pathLength = progressPath.getTotalLength();
+
+  progressPath.style.transition = progressPath.style.WebkitTransition = "none";
+  progressPath.style.strokeDasharray = pathLength + " " + pathLength;
+  progressPath.style.strokeDashoffset = pathLength;
+  progressPath.getBoundingClientRect();
+  progressPath.style.transition = progressPath.style.WebkitTransition =
+    "stroke-dashoffset 10ms linear";
+
+  const updateProgress = function () {
+    const scroll = window.scrollY;
+    const height = document.documentElement.scrollHeight - window.innerHeight;
+    const progress = pathLength - (scroll * pathLength) / height;
+    progressPath.style.strokeDashoffset = progress;
+  };
+
+  updateProgress();
+  window.addEventListener("scroll", updateProgress);
+
+  const offset = 150;
+  const progressWrap = document.querySelector(".progress-wrap");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > offset) {
+      progressWrap.classList.add("active-progress");
+    } else {
+      progressWrap.classList.remove("active-progress");
+    }
+  });
+
+  progressWrap.addEventListener("click", function (event) {
+    event.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
 });
 
 // --- 3. HEADER SCROLL LOGIC ---
