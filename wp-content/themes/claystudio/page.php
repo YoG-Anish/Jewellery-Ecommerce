dc5da64c91734f1f2f531caa4a4632429268bdad
Sabin Prajapati <sabinprajapati7@gmail.com>
2026-06-04 10:05:26 +0545

    Css Improvement

--- wp-content/themes/claystudio/page.php
+++ wp-content/themes/claystudio/page.php
@@ -1,16 +1,18 @@
 <?php
 get_header();
 ?>
-<div class="container">
-    
-<?php
-if ( have_posts() ) {
-    while ( have_posts() ) {
-        the_post();
-        the_content();
-    }
-}
-?>
-</div>
+<section class="shop-all-collection-section">
+    <div class="container">
+
+        <?php
+        if (have_posts()) {
+            while (have_posts()) {
+                the_post();
+                the_content();
+            }
+        }
+        ?>
+    </div>
+</section>
 <?php
 get_footer();
